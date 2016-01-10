0ae5677bb642e40c436e5a4ef9ae4a2319309da6
vijayanand14 <bhupathi.vijayanand@gmail.com>
2016-01-11 03:33:45 +0530

    Create weekdayweekend.R

---
+++ weekdayweekend.R
@@ -0,0 +1,10 @@
+data_full <- mutate(data_full, weektype = ifelse(weekdays(data_full$date) == "Saturday" | weekdays(data_full$date) == "Sunday", "weekend", "weekday"))
+data_full$weektype <- as.factor(data_full$weektype)
+head(data_full)
+interval_full <- data_full %>%
+  group_by(interval, weektype) %>%
+  summarise(steps = mean(steps))
+s <- ggplot(interval_full, aes(x=interval, y=steps, color = weektype)) +
+  geom_line() +
+  facet_wrap(~weektype, ncol = 1, nrow=2)
+print(s)
