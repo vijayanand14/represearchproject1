005a0c7c33ed0cfce96651d52a39ebaaa8885840
vijayanand14 <bhupathi.vijayanand@gmail.com>
2016-01-11 03:48:12 +0530

    Delete averagesteps.R

--- averagesteps.R
+++
@@ -1,8 +0,0 @@
-interval <- data %>%
-  filter(!is.na(steps)) %>%
-  group_by(interval) %>%
-  summarize(steps = mean(steps))
-  ggplot(interval, aes(x=interval, y=steps)) +
-  geom_line(color = "firebrick")
-  interval[which.max(interval$steps),]
-  
